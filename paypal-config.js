fc31fa544cbe4494a2addeced7a68d7ba33e567e
KornDog0804 <titans.rule1215@gmail.com>
2025-08-27 13:55:03 -0500

    Delete paypal-config.js

--- paypal-config.js
+++
@@ -1,7 +0,0 @@
-
-export default async ()=>{
-  return Response.json({
-    clientId: process.env.PAYPAL_CLIENT_ID || process.env.PAYPAL_PUBLIC_CLIENT_ID || '',
-    currency: process.env.PAYPAL_CURRENCY || 'USD'
-  });
-}
